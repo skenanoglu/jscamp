680269db36205ca350b94b205e4a7445e79ba479
skenanoglu <kenanoglu115@gmail.com>
2021-10-19 00:21:28 +0300

    Deneme

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("merhaba kodlama io")
+let konutKredileri=["konut kredisi","emlak konut kredisi","kamu konut kredisii"]
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
